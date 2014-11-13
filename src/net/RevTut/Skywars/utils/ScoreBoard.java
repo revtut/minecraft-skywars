e89b22529b2d3518fa775f6d7ce56473cd07e818
RevTut <jolivsilva2095@gmail.com>
2014-11-13 18:38:05 +0000

    Created JAVADOCS in "utils" package.

--- src/net/RevTut/Skywars/utils/ScoreBoard.java
+++ src/net/RevTut/Skywars/utils/ScoreBoard.java
@@ -9,21 +9,43 @@ import org.bukkit.event.Listener;
 import org.bukkit.scoreboard.*;
 
 import java.util.HashMap;
+import java.util.Map;
 import java.util.UUID;
 
+/**
+ * ScoreBoard.
+ *
+ * <P>Class to handle all the scoreboards in the server.</P>
+ *
+ * @author Joao Silva
+ * @version 1.0
+ */
 public class ScoreBoard implements Listener {
+
+    /** Main class */
     private final Main plugin;
 
+    /**
+     * Constructor of ScoreBoard
+     *
+     * @param plugin        main class
+     */
     public ScoreBoard(final Main plugin) {
         this.plugin = plugin;
     }
 
-    /* ScoreBoards */
-    private static final HashMap<UUID, Scoreboard> scoreBoards = new HashMap<UUID, Scoreboard>();
+    /** Map with all the scoreboards of the players */
+    private static final Map<UUID, Scoreboard> scoreBoards = new HashMap<UUID, Scoreboard>();
 
-    /* Show ScoreBoard to Player */
+    /**
+     * Show a new scoreboard to a player
+     *
+     * @param p         player to show the scoreboard
+     * @param arena     arena where the player is
+     * @see             Arena
+     */
     public static void showScoreBoard(Player p, Arena arena) {
-        /* Cria a scoreboard */
+        // Create the scoreboard
         ScoreboardManager manager = Bukkit.getScoreboardManager();
         Scoreboard board = manager.getNewScoreboard();
         Objective objective = board.registerNewObjective("test", "dummy");
@@ -31,27 +53,35 @@ public class ScoreBoard implements Listener {
         objective.setDisplaySlot(DisplaySlot.SIDEBAR);
         objective.setDisplayName("§7► §3Sky Wars §7◄");
 
-        /* Alive */
+        // Alive
         final Score scoreAlive = objective.getScore("§aAlive:");
         scoreAlive.setScore(0);
 
-        /* Dead */
+        // Dead
         final Score scoreDead = objective.getScore("§cDead:");
         scoreDead.setScore(0);
 
-        /* Scoreboard footer */
+        // Scoreboard footer
         final Score separador = objective.getScore("§3----------");
         separador.setScore(-1);
 
+        // Advertisement
         final Score website = objective.getScore("§7Website:");
         website.setScore(-2);
         final Score site = objective.getScore("§3revtut.net");
         site.setScore(-3);
         p.setScoreboard(board);
+
+        // Add to the map
         scoreBoards.put(p.getUniqueId(), board);
     }
 
-    /* Update Alive */
+    /**
+     * Update alive players in the scoreboard of all the
+     * players which are on that arena
+     *
+     * @param arena     arena to update alive players
+     */
     public static void updateAlive(Arena arena) {
         int alive = arena.getAlivePlayers().size();
         for (PlayerDat alvoDat : arena.getPlayers()) {
@@ -66,7 +96,12 @@ public class ScoreBoard implements Listener {
         }
     }
 
-    /* Update Death */
+    /**
+     * Update death players in the scoreboard of all the
+     * players which are on that arena
+     *
+     * @param arena     arena to update death players
+     */
     public static void updateDeath(Arena arena) {
         int dead = arena.getDeadPlayers().size();
         for (PlayerDat alvoDat : arena.getPlayers()) {
@@ -81,14 +116,24 @@ public class ScoreBoard implements Listener {
         }
     }
 
-    /* Get Player's ScoreBoard */
+    /**
+     * Get the scoreboard of a given player
+     *
+     * @param uuid      uuid of the player
+     * @return          scoreboard of the player
+     */
     public static Scoreboard getScoreBoardByPlayer(UUID uuid) {
         if (scoreBoards.containsKey(uuid))
             return scoreBoards.get(uuid);
         return null;
     }
 
-    /* Remove ScoreBoard From Map */
+    /**
+     * Remove the scoreboard of a player from the Map
+     *
+     * @param player        player which scoreboard will be removed
+     * @return              true if successfull
+     */
     public static boolean removePlayerScoreBoard(Player player) {
         if (scoreBoards.containsKey(player.getUniqueId())) {
             scoreBoards.remove(player.getUniqueId());
