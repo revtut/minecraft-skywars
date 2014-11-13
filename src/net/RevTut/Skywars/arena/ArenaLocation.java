1f290b9429b539746d047714494a77bea558d3a6
RevTut <jolivsilva2095@gmail.com>
2014-11-13 15:48:36 +0000

    Added JAVADOCS to ArenaLocation.

--- src/net/RevTut/Skywars/arena/ArenaLocation.java
+++ src/net/RevTut/Skywars/arena/ArenaLocation.java
@@ -6,18 +6,39 @@ import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Created by João on 31/10/2014.
+ * ArenaLocation Object.
+ *
+ * <P>A complement to Arena Object, which saves all the information regarding game locations.</P>
+ *
+ * @author Joao Silva
+ * @version 1.0
  */
 public class ArenaLocation {
 
-    /* Arena Location */
+    /** Lobby location */
     private final Location lobbyLocation;
-    private Location deathSpawnLocation; // Location where death player spawn
-    private Location firstCorner; // Limits of the arena
-    private Location secondCorner; // Limits of the arena
-    private List<Location> spawnLocations = new ArrayList<Location>(); // Spawn player locations
 
-    /* Constructor */
+    /** Location where dead players will spawn */
+    private Location deathSpawnLocation;
+
+    /** Inferior limit of the arena (minimum coords) */
+    private Location firstCorner;
+
+    /** Superior limit of the arena (maximum coords) */
+    private Location secondCorner;
+
+    /** List with all the spawn locations of the game */
+    private List<Location> spawnLocations = new ArrayList<Location>();
+
+    /**
+     * Constructor of ArenaLocation
+     *
+     * @param lobbyLocation             lobby location
+     * @param deathSpawnLocation        death spawn location
+     * @param firstCorner               first corner of the arena (min coords)
+     * @param secondCorner              second corner of the arena (max coords)
+     * @param spawnLocations            spawn locations of the game
+     */
     public ArenaLocation(Location lobbyLocation, Location deathSpawnLocation, Location firstCorner, Location secondCorner, List<Location> spawnLocations) {
         this.lobbyLocation = lobbyLocation;
         this.deathSpawnLocation = deathSpawnLocation;
@@ -26,40 +47,63 @@ public class ArenaLocation {
         this.spawnLocations = spawnLocations;
     }
 
-    /* Get's */
+    /** Returns the lobby location of the arena */
     public Location getLobbyLocation() {
         return lobbyLocation;
     }
 
+    /** Returns the death spawn location of the arena */
     public Location getDeathSpawnLocation() {
         return deathSpawnLocation;
     }
 
+    /** Returns the first corner of the arena (minimum coords) */
     public Location getFirstCorner() {
         return firstCorner;
     }
 
+    /** Returns the second corner of the arena (maximum coords) */
     public Location getSecondCorner() {
         return secondCorner;
     }
 
+    /** Returns list with the spawn locations of the game */
     public List<Location> getSpawnLocations() {
         return spawnLocations;
     }
 
-    /* Set's */
+    /**
+     * Set the death spawn location of the arena
+     *
+     * @param deathSpawnLocation        the death spawn location
+     */
     public void setDeathSpawnLocation(Location deathSpawnLocation) {
         this.deathSpawnLocation = deathSpawnLocation;
     }
 
+    /**
+     * Set the first corner location of the arena
+     *
+     * @param firstCorner       the first corner of the arena
+     */
     public void setFirstCorner(Location firstCorner) {
         this.firstCorner = firstCorner;
     }
 
+    /**
+     * Set the second corner location of the arena
+     *
+     * @param secondCorner      the second corner of the arena
+     */
     public void setSecondCorner(Location secondCorner) {
         this.secondCorner = secondCorner;
     }
 
+    /**
+     * Set the spawn locations of the arena
+     *
+     * @param spawnLocations        list with all the spawn locations
+     */
     public void setSpawnLocations(List<Location> spawnLocations) {
         this.spawnLocations = spawnLocations;
     }
