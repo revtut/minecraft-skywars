7333ec734c2115118acdf27d6d3ab195924b0412
RevTut <jolivsilva2095@gmail.com>
2014-11-14 15:38:32 +0000

    Added PlayerRespawnEvent.

--- src/net/RevTut/Skywars/listeners/PlayerRespawn.java
+++ src/net/RevTut/Skywars/listeners/PlayerRespawn.java
@@ -1,6 +1,9 @@
 package net.RevTut.Skywars.listeners;
 
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerRespawnEvent;
 
 /**
  * Player Respawn.
@@ -11,5 +14,18 @@ import org.bukkit.event.Listener;
  * @version 1.0
  */
 public class PlayerRespawn implements Listener {
+
+    /**
+     * Takes care of a player when he respawns
+     *
+     * @param e     player respawn event
+     * @see         PlayerRespawnEvent
+     */
+    @EventHandler
+    public void onPlayerRespawn(PlayerRespawnEvent e){
+        Player player = e.getPlayer();
+        // Send to spawn
+        player.teleport(player.getWorld().getSpawnLocation());
+    }
 
 }
