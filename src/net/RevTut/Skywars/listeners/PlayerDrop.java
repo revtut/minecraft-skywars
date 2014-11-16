8ee80d168ae41daf913bd7eb15d0dfac0a1f14b4
RevTut <jolivsilva2095@gmail.com>
2014-11-16 00:37:42 +0000

    Added drop and pickup event.

---
+++ src/net/RevTut/Skywars/listeners/PlayerDrop.java
@@ -0,0 +1,62 @@
+package net.RevTut.Skywars.listeners;
+
+import net.RevTut.Skywars.Main;
+import net.RevTut.Skywars.arena.Arena;
+import net.RevTut.Skywars.arena.ArenaStatus;
+import net.RevTut.Skywars.player.PlayerDat;
+import net.RevTut.Skywars.player.PlayerStatus;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
+
+/**
+ * Player Drop.
+ *
+ * <P>Controls the drop item event.</P>
+ *
+ * @author WaxCoder
+ * @version 1.0
+ */
+public class PlayerDrop implements Listener {
+
+    /**
+     * Main class
+     */
+    private final Main plugin;
+
+    /**
+     * Constructor of PlayerDrop
+     *
+     * @param plugin main class
+     */
+    public PlayerDrop(final Main plugin) {
+        this.plugin = plugin;
+    }
+
+    /**
+     * Takes care of a player when he drops an item
+     *
+     * @param e     player drop item event
+     * @see         PlayerDropItemEvent
+     */
+    @EventHandler
+    public void onPlayerDropItem(PlayerDropItemEvent e){
+        Player player = e.getPlayer();
+        // Player Dat
+        PlayerDat playerDat = PlayerDat.getPlayerDatByUUID(player.getUniqueId());
+        if(playerDat == null)
+            return;
+        // Arena
+        Arena arena = plugin.arenaManager.getArenaByPlayer(playerDat);
+        if(null == arena)
+            return;
+        if(arena.getStatus() != ArenaStatus.INGAME)
+            e.setCancelled(true);
+        if(playerDat.getStatus() != PlayerStatus.ALIVE)
+            e.setCancelled(true);
+    }
+
+}
---
+++ src/net/RevTut/Skywars/listeners/PlayerPickup.java
@@ -0,0 +1,61 @@
+package net.RevTut.Skywars.listeners;
+
+import net.RevTut.Skywars.Main;
+import net.RevTut.Skywars.arena.Arena;
+import net.RevTut.Skywars.arena.ArenaStatus;
+import net.RevTut.Skywars.player.PlayerDat;
+import net.RevTut.Skywars.player.PlayerStatus;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerDropItemEvent;
+import org.bukkit.event.player.PlayerPickupItemEvent;
+
+/**
+ * Player Pickup.
+ *
+ * <P>Controls the pickup item event.</P>
+ *
+ * @author WaxCoder
+ * @version 1.0
+ */
+public class PlayerPickup implements Listener {
+
+    /**
+     * Main class
+     */
+    private final Main plugin;
+
+    /**
+     * Constructor of PlayerPickup
+     *
+     * @param plugin main class
+     */
+    public PlayerPickup(final Main plugin) {
+        this.plugin = plugin;
+    }
+
+    /**
+     * Takes care of a player when he pickups an item
+     *
+     * @param e     player pickup item event
+     * @see         PlayerPickupItemEvent
+     */
+    @EventHandler
+    public void onPlayerPickupItem(PlayerPickupItemEvent e){
+        Player player = e.getPlayer();
+        // Player Dat
+        PlayerDat playerDat = PlayerDat.getPlayerDatByUUID(player.getUniqueId());
+        if(playerDat == null)
+            return;
+        // Arena
+        Arena arena = plugin.arenaManager.getArenaByPlayer(playerDat);
+        if(null == arena)
+            return;
+        if(arena.getStatus() != ArenaStatus.INGAME)
+            e.setCancelled(true);
+        if(playerDat.getStatus() != PlayerStatus.ALIVE)
+            e.setCancelled(true);
+    }
+
+}
