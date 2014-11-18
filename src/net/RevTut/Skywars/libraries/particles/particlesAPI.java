7d2edd5c25b5513adfc8cc06539b20924190842c
waxcoder <waxcoder3@gmail.com>
2014-11-18 17:20:54 +0100

    Kit Guardian 're-upload' , lib Particles 'organized'

--- src/net/RevTut/Skywars/libraries/particles/particlesAPI.java
+++ src/net/RevTut/Skywars/libraries/particles/particlesAPI.java
@@ -7,18 +7,28 @@ import org.bukkit.craftbukkit.v1_7_R4.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 
 /**
- * Created by waxcoder on 18-11-2014.
+ * Particle API.
+ *
+ * <P>Help make particles with simple methods</P>
+ *
+ * @author WaxCoder
+ * @version 1.0
  */
+
 public class particlesAPI {
 
-    /* Create helix effect */
+    /**
+     * Create helix particle effect using packets
+     *
+     * @param player player to create helix effect
+     */
     public static void createHelix(Player player) {
         Location loc = player.getLocation();
         int radius = 2;
         for(double y = 0; y <= 50; y+=0.05) {
             double x = radius * Math.cos(y);
             double z = radius * Math.sin(y);
-            PacketPlayOutWorldParticles packet = new PacketPlayOutWorldParticles("fireworksSpark", (float) (loc.getX() + x), (float) (loc.getY() + y), (float) (loc.getZ() + z), 0, 0, 0, 0, 1);
+            PacketPlayOutWorldParticles packet = new PacketPlayOutWorldParticles("fireworksSpark", (float) (loc.getX() + x), (float) (loc.getY() + y ), (float) (loc.getZ() + z), 0, 0, 0, 0, 1);
             for(Player online : Bukkit.getOnlinePlayers()) {
                 ((CraftPlayer)online).getHandle().playerConnection.sendPacket(packet);
             }
