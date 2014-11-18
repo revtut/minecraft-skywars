db00fa9e2cba6c13b9a02cd3556fe0e632532b0a
RevTut <jolivsilva2095@gmail.com>
2014-11-18 18:00:51 +0000

    Created ParticlesAPI.

---
+++ src/net/RevTut/Skywars/libraries/particles/ParticlesAPI.java
@@ -0,0 +1,39 @@
+package net.RevTut.Skywars.libraries.particles;
+
+import net.minecraft.server.v1_7_R4.PacketPlayOutWorldParticles;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.craftbukkit.v1_7_R4.entity.CraftPlayer;
+import org.bukkit.entity.Player;
+
+/**
+ * Particle API.
+ *
+ * <P>Help make particles with simple methods</P>
+ *
+ * @author WaxCoder
+ * @version 1.0
+ */
+
+public class ParticlesAPI {
+
+    /**
+     * Create helix particle effect using particles packet
+     *
+     * @param player player to create helix effect
+     */
+    public static void createHelix(Player player) {
+        Location loc = player.getLocation();
+        int radius = 2;
+        for(double y = 0; y <= 50; y+=0.05) {
+            double x = radius * Math.cos(y);
+            double z = radius * Math.sin(y);
+            PacketPlayOutWorldParticles packet = new PacketPlayOutWorldParticles("fireworksSpark", (float) (loc.getX() + x), (float) (loc.getY() + y ), (float) (loc.getZ() + z), 0, 0, 0, 0, 1);
+            for(Player online : Bukkit.getOnlinePlayers()) {
+                ((CraftPlayer)online).getHandle().playerConnection.sendPacket(packet);
+            }
+        }
+    }
+
+
+}
