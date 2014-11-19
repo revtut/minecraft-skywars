7cc3f9ce315f42d90af9b3304ca35ec11d1ad47c
waxcoder <waxcoder3@gmail.com>
2014-11-19 20:32:23 +0100

    ScreamerKit

--- src/net/RevTut/Skywars/kits/Screamer.java
+++ src/net/RevTut/Skywars/kits/Screamer.java
@@ -1,6 +1,7 @@
 package net.RevTut.Skywars.kits;
 
 import net.RevTut.Skywars.libraries.particles.ParticlesAPI;
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.entity.Arrow;
 import org.bukkit.entity.Player;
@@ -9,26 +10,46 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.ProjectileHitEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.util.Vector;
 
 /**
  * Created by waxcoder on 18-11-2014.
  */
 public class Screamer implements Listener {
 
-    private static ItemStack arrowScreamer = new ItemStack(Material.ARROW, 32);
-
-    /* Create projectile with particle effect helix */
-    @EventHandler
-    public void projectile(ProjectileHitEvent e){
-        Projectile proj = e.getEntity();
-        Arrow arrow = (Arrow) proj;
-        if(proj instanceof Arrow){
-            Player p = (Player) arrow.getShooter();
-            ParticlesAPI.helixPosX(p);
-        }
+    private static ItemStack ScreamerItem = new ItemStack(Material.FIREBALL, 1);{
+        ItemMeta ScreamerItemMeta = ScreamerItem.getItemMeta();
+        ScreamerItemMeta.setDisplayName("§3Scream!!");
+        ScreamerItem.setItemMeta(ScreamerItemMeta);
     }
 
+    /* Create projectile with particle effect helix */
+
     public void kitScreamer(Player p) {
-        p.getInventory().addItem(arrowScreamer);
+        p.getInventory().addItem(ScreamerItem);
+    }
+
+    public void pullPlayer(Player player, ItemStack itemStack, Projectile projectile) { // MAKES USE OF PROJECTILE HIT EVENT
+        if (itemStack == null)
+            return;
+        if (itemStack.getType() == null)
+            return;
+        if (itemStack.getType() != Material.FIREBALL)
+            return;
+        if (!itemStack.hasItemMeta())
+            return;
+        if (!itemStack.getItemMeta().hasDisplayName())
+            return;
+        if (!itemStack.getItemMeta().getDisplayName().equalsIgnoreCase("§3Scream!!"))
+            return;
+
+
+        // Remove item
+        int amount = itemStack.getAmount();
+        if(amount > 1)
+            itemStack.setAmount(itemStack.getAmount() - 1);
+        else
+            player.getInventory().remove(itemStack);
     }
 }
