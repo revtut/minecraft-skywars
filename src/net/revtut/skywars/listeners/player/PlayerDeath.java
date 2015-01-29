dd413c6fc82a2e76a521f616794f0e2157843dd6
RevTut <joaosilva@revtut.net>
2015-01-29 16:43:48 +0000

    Optimized code on Player Death.

--- src/net/revtut/skywars/listeners/player/PlayerDeath.java
+++ src/net/revtut/skywars/listeners/player/PlayerDeath.java
@@ -60,32 +60,27 @@ public class PlayerDeath implements Listener {
         if (alvoDat == null)
             return;
 
-        // Arena target
-        Arena alvoArena = plugin.arenaManager.getArenaByPlayer(alvoDat);
-        if (alvoArena == null)
+        // Arena
+        Arena arena = plugin.arenaManager.getArenaByPlayer(alvoDat);
+        if (arena == null)
             return;
 
         // ArenaDat
-        ArenaDat arenaDat = alvoArena.getArenaDat();
+        ArenaDat arenaDat = arena.getArenaDat();
         if (arenaDat == null) {
             plugin.getLogger().log(Level.WARNING, "ArenaDat is null when player dies!");
             return;
         }
 
         // Hacker
-        if (alvoArena.getKitManager().hacker.canRespawn(alvoDat, alvoArena)){
+        if (arena.getKitManager().hacker.canRespawn(alvoDat, arena)){
             // Bypass respawn
             BypassesAPI.respawnBypass(alvo);
             return;
         }
 
-        // Update player
-        alvoDat.setStatus(PlayerStatus.DEAD);
-        alvo.setGameMode(GameMode.SPECTATOR);
-
-        // Update target stats
-        alvoDat.addDeath();
-        alvoDat.addLose();
+        // Respawn
+        Bukkit.getScheduler().runTaskLater(plugin, () -> BypassesAPI.respawnBypass(alvo), 20L);
 
         // Hide to Arena
         plugin.arenaManager.hideToArena(alvo, false);
@@ -94,83 +89,31 @@ public class PlayerDeath implements Listener {
         plugin.arenaManager.showToSpectators(alvo, true);
 
         // Scoreboard update alive players and dead
-        plugin.scoreBoardManager.updateAlive(alvoArena);
-        plugin.scoreBoardManager.updateDeath(alvoArena);
-
-        // Update stats
-        plugin.scoreBoardManager.updateWinsLosses(alvoDat);
-        plugin.scoreBoardManager.updateKillsDeaths(alvoDat);
-
-        // Respawn
-        Bukkit.getScheduler().runTaskLater(plugin, () -> BypassesAPI.respawnBypass(alvo), 20L);
+        plugin.scoreBoardManager.updateAlive(arena);
+        plugin.scoreBoardManager.updateDeath(arena);
 
         // Damager player
-        Player damager = null;
-        PlayerDat damagerDat = null;
-        if (PlayerDamage.lastPlayerDamager.containsKey(alvo.getUniqueId())) {
-            damager = Bukkit.getPlayer(PlayerDamage.lastPlayerDamager.get(alvo.getUniqueId()));
-            if(damager != null){
-                damagerDat = plugin.playerManager.getPlayerDatByUUID(damager.getUniqueId());
-                if (damagerDat != null)
-                    if(!plugin.arenaManager.inSameArena(alvoDat, damagerDat))
-                        return;
-            }
-            PlayerDamage.lastPlayerDamager.remove(alvo.getUniqueId());
-        }
+        Player damager = getLastDamager(alvo);
 
         // Messages
-        if (damager != null && damagerDat != null) {
-            // Points earned
-            int poinsEarned = (int) (plugin.pointsPerKill + plugin.pointsPerKill * ((float) damagerDat.getGameKills() / arenaDat.getInitialPlayers().size()) + plugin.pointsPerKill * ((float) plugin.rand.nextInt(11) / 100));
-            damagerDat.addPoints(poinsEarned);
-
-            // Add kill
-            damagerDat.addKill();
-
-            // Update stats
-            plugin.scoreBoardManager.updateKillsDeaths(damagerDat);
-
-            // Message to arena
-            alvoArena.sendMessage(Message.PLAYER_DIED, alvo.getName());
-            alvoArena.sendMessage(Message.PLAYER_KILLED_BY, damager.getName());
-            arenaDat.addGameEvent(ChatColor.stripColor(alvo.getName() + " was killed by " + damager.getName() + ".")); // Add to event log
-
-            // Multi Kills
-            int numberKills = damagerDat.getGameKills();
-            sendMultiKillMessage(numberKills, alvoArena, damager);
-
-            // Target
-            TitleAPI.sendTimes(alvo, 5, 60, 5);
-            TitleAPI.sendTitle(alvo, ConvertersAPI.convertToJSON(Message.getMessage(Message.YOU_DIED, alvo)));
-            TitleAPI.sendSubTitle(alvo, ConvertersAPI.convertToJSON(Message.getMessage(Message.YOU_WERE_KILLED_BY, alvo) + damager.getName()));
-
-            // Damager
-            TitleAPI.sendTimes(damager, 5, 60, 5);
-            TitleAPI.sendTitle(damager, ConvertersAPI.convertToJSON(Message.getMessage(Message.YOU_KILLED, damager)));
-            TitleAPI.sendSubTitle(damager, ConvertersAPI.convertToJSON("§7" + alvo.getName()));
+        if (damager != null) {
+            killedByPlayer(arena, alvo, damager);
         } else {
-            // Message to arena
-            alvoArena.sendMessage(Message.PLAYER_DIED, alvo.getName());
-            arenaDat.addGameEvent(ChatColor.stripColor(alvo.getName() + " died.")); // Add to event log
-
-            // Target
-            TitleAPI.sendTimes(alvo, 5, 60, 5);
-            TitleAPI.sendTitle(alvo, ConvertersAPI.convertToJSON(Message.getMessage(Message.YOU_DIED, alvo)));
-            TitleAPI.sendSubTitle(alvo, ConvertersAPI.convertToJSON(""));
+            killedByNature(arena, alvo);
         }
 
         // Check if game ended
-        if (alvoArena.getAlivePlayers().size() <= 1) {
-            if(alvoArena.getAlivePlayers().size() == 1){
+        if (arena.getAlivePlayers().size() <= 1) {
+            if(arena.getAlivePlayers().size() == 1){
                 // Winner Dat
-                PlayerDat winnerDat = alvoArena.getAlivePlayers().get(0);
+                PlayerDat winnerDat = arena.getAlivePlayers().get(0);
                 if (winnerDat != null){
                     arenaDat.setWinner(winnerDat.getUUID().toString());
                     winnerDat.setStatus(PlayerStatus.WAITING); // Set status so he cannot die
                 }
             }
             // Set Remaining Time
-            alvoArena.setRemainingTime(0);
+            arena.setRemainingTime(0);
         }
     }
 
@@ -222,5 +165,108 @@ public class PlayerDeath implements Listener {
                 arenaDat.addGameEvent(ChatColor.stripColor(damager.getName() + " unbelievable kill!")); // Add to event log
                 break;
         }
+    }
+
+    private void updateKilledPlayer(Arena arena, Player target) {
+        final PlayerDat targetDat = plugin.playerManager.getPlayerDatByUUID(target.getUniqueId());
+        if (targetDat == null)
+            return;
+
+        // Update player
+        targetDat.setStatus(PlayerStatus.DEAD);
+        target.setGameMode(GameMode.SPECTATOR);
+
+        // Update target stats
+        targetDat.addDeath();
+        targetDat.addLose();
+
+        // Update stats
+        plugin.scoreBoardManager.updateWinsLosses(targetDat);
+        plugin.scoreBoardManager.updateKillsDeaths(targetDat);
+    }
+
+    /**
+     * Take care of what to do when a player is killed by another player
+     *
+     * @param arena arena where player was killed
+     * @param target player that was killed
+     * @param damager player that killed
+     */
+    private void killedByPlayer(Arena arena, Player target, Player damager) {
+        PlayerDat damagerDat = plugin.playerManager.getPlayerDatByUUID(damager.getUniqueId());
+        if(damagerDat == null)
+            return;
+
+        // Points earned
+        int poinsEarned = (int) (plugin.pointsPerKill + plugin.pointsPerKill * ((float) damagerDat.getGameKills() / arena.getArenaDat().getInitialPlayers().size()) + plugin.pointsPerKill * ((float) plugin.rand.nextInt(11) / 100));
+        damagerDat.addPoints(poinsEarned);
+
+        // Add kill
+        damagerDat.addKill();
+
+        // Update stats
+        plugin.scoreBoardManager.updateKillsDeaths(damagerDat);
+
+        // Message to arena
+        arena.sendMessage(Message.PLAYER_DIED, target.getName());
+        arena.sendMessage(Message.PLAYER_KILLED_BY, damager.getName());
+        arena.getArenaDat().addGameEvent(ChatColor.stripColor(target.getName() + " was killed by " + damager.getName() + ".")); // Add to event log
+
+        // Multi Kills
+        int numberKills = damagerDat.getGameKills();
+        sendMultiKillMessage(numberKills, arena, damager);
+
+        // Target
+        TitleAPI.sendTimes(target, 5, 60, 5);
+        TitleAPI.sendTitle(target, ConvertersAPI.convertToJSON(Message.getMessage(Message.YOU_DIED, target)));
+        TitleAPI.sendSubTitle(target, ConvertersAPI.convertToJSON(Message.getMessage(Message.YOU_WERE_KILLED_BY, target) + damager.getName()));
+
+        // Damager
+        TitleAPI.sendTimes(damager, 5, 60, 5);
+        TitleAPI.sendTitle(damager, ConvertersAPI.convertToJSON(Message.getMessage(Message.YOU_KILLED, damager)));
+        TitleAPI.sendSubTitle(damager, ConvertersAPI.convertToJSON("§7" + target.getName()));
+    }
+
+    /**
+     * Take care of what to do when a player is killed by nature
+     *
+     * @param arena arena where player was killed
+     * @param target killed player
+     */
+    private void killedByNature(Arena arena, Player target) {
+        // Message to arena
+        arena.sendMessage(Message.PLAYER_DIED, target.getName());
+        arena.getArenaDat().addGameEvent(ChatColor.stripColor(target.getName() + " died.")); // Add to event log
+
+        // Target
+        TitleAPI.sendTimes(target, 5, 60, 5);
+        TitleAPI.sendTitle(target, ConvertersAPI.convertToJSON(Message.getMessage(Message.YOU_DIED, target)));
+        TitleAPI.sendSubTitle(target, ConvertersAPI.convertToJSON(""));
+    }
+
+    /**
+     * Get the last damager of a player
+     *
+     * @param target player to get the last damager
+     * @return last damager
+     */
+    private Player getLastDamager(Player target) {
+        final PlayerDat alvoDat = plugin.playerManager.getPlayerDatByUUID(target.getUniqueId());
+        if (alvoDat == null)
+            return null;
+
+        Player damager = null;
+        PlayerDat damagerDat;
+        if (PlayerDamage.lastPlayerDamager.containsKey(target.getUniqueId())) {
+            damager = Bukkit.getPlayer(PlayerDamage.lastPlayerDamager.get(target.getUniqueId()));
+            if(damager != null){
+                damagerDat = plugin.playerManager.getPlayerDatByUUID(damager.getUniqueId());
+                if (damagerDat != null)
+                    if(!plugin.arenaManager.inSameArena(alvoDat, damagerDat))
+                        return null;
+            }
+            PlayerDamage.lastPlayerDamager.remove(target.getUniqueId());
+        }
+        return damager;
     }
 }
