7796651c86d41ae4a81553a68ec2a60fe635e5ed
RevTut <joaosilva@revtut.net>
2015-10-07 09:37:19 +0100

    Set the information's board title as scrolling label.

--- src/net/revtut/skywars/InfoBoardManager.java
+++ src/net/revtut/skywars/InfoBoardManager.java
@@ -3,7 +3,12 @@ package net.revtut.skywars;
 import net.revtut.libraries.games.arena.Arena;
 import net.revtut.libraries.games.player.PlayerState;
 import net.revtut.libraries.scoreboard.InfoBoard;
+import net.revtut.libraries.scoreboard.label.BlankLabel;
+import net.revtut.libraries.scoreboard.label.ScrollingLabel;
 import net.revtut.libraries.scoreboard.label.StaticLabel;
+import net.revtut.libraries.text.scroller.ColorScroller;
+import net.revtut.libraries.text.scroller.TextScroller;
+import org.bukkit.ChatColor;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -49,22 +54,24 @@ public class InfoBoardManager {
      * @return created information board
      */
     public InfoBoard createInfoBoard(Arena arena) {
-        InfoBoard infoBoard = new InfoBoard();
+        InfoBoard infoBoard = new InfoBoard(5);
 
         // Title
-        infoBoard.setTitle("§bSKY WARS");
+        infoBoard.setTitle(new ScrollingLabel(new ColorScroller("SKY WARS", 1, ChatColor.AQUA, ChatColor.GOLD)));
 
         // Body
-        infoBoard.addLabel(new StaticLabel("   ", 10));
+        infoBoard.addLabel(new BlankLabel(12));
+        infoBoard.addLabel(new ScrollingLabel(new TextScroller("§fBecome §6VIP §fat our store! | ", 15), 11));
+        infoBoard.addLabel(new BlankLabel(10));
         infoBoard.addLabel(new StaticLabel("§6Session: §f" + arena.getId(), 9));
         infoBoard.addLabel(new StaticLabel("§6Map: §f" + arena.getWorld().getName().split("_")[2], 8));
-        infoBoard.addLabel(new StaticLabel("  ", 7));
+        infoBoard.addLabel(new BlankLabel(7));
         infoBoard.addLabel(new StaticLabel("§aAlive: §f" + arena.getPlayers(PlayerState.ALIVE).size(), 6));
         infoBoard.addLabel(new StaticLabel("§cDead: §f" + arena.getPlayers(PlayerState.DEAD).size(), 5));
-        infoBoard.addLabel(new StaticLabel(" ", 4));
+        infoBoard.addLabel(new BlankLabel(4));
 
         // Footer
-        infoBoard.addLabel(new StaticLabel("§7----------------", 3));
+        infoBoard.addLabel(new StaticLabel("§8----------------", 3));
         infoBoard.addLabel(new StaticLabel("§fCheck us out", 1));
         infoBoard.addLabel(new StaticLabel("§6www.revtut.net", 0));
         return infoBoard;
